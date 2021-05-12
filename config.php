af2bca12f0e6e8409999694ac0327cd3c6f458f3
NielsDewolfPXL <43777476+NielsDewolfPXL@users.noreply.github.com>
2021-05-12 15:48:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     require('vendor/autoload.php');
     // Define mysql server settings
-    $serverName = "localhost";
-    $username = "root";
-    $password = "";
-    $dbName = "employees";
+    $serverName = "database-1.c1nxcud0u6wb.us-east-1.rds.amazonaws.com";
+    $username = "groep7";
+    $password = "R,Oalx:ZQ.0P";
+    $dbName = "database-1";
 
     // Create a new mysqli connection. Remember to enable this in php.ini !!
     $conn = new mysqli($serverName, $username, $password, $dbName);
