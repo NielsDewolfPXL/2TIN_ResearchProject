76a13e055534ef393b11ddab110cc56afabd454d
NielsDewolfPXL <43777476+NielsDewolfPXL@users.noreply.github.com>
2021-05-13 16:14:25 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,12 +5,17 @@
     //$username = "groep7";
     //$password = "R,Oalx:ZQ.0P";
     //$dbName = "database-1";
-    $RDS_HOSTNAME = "database-1.c1nxcud0u6wb.us-east-1.rds.amazonaws.com";
-    $RDS_PORT = "3306";
-    $RDS_DB_NAME = "dbgroup7";
-    $RDS_USERNAME = "groep7";
-    $RDS_PASSWORD = "R,Oalx:ZQ.0P";
-
     // Create a new mysqli connection. Remember to enable this in php.ini !!
     //$conn = new mysqli($serverName, $username, $password, $dbName);
-    $conn = new mysqli($RDS_HOSTNAME, $RDS_PORT, $RDS_DB_NAME, $RDS_USERNAME, $RDS_PASSWORD);
+    //$conn = new mysqli($RDS_HOSTNAME, $RDS_PORT, $RDS_DB_NAME, $RDS_USERNAME, $RDS_PASSWORD);
+
+    $dbhost = $_SERVER['database-1.c1nxcud0u6wb.us-east-1.rds.amazonaws.com'];
+    $dbport = $_SERVER['3306'];
+    $dbname = $_SERVER['dbgroup7'];
+    $charset = 'utf8' ;
+
+    $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+    $username = $_SERVER['groep7'];
+    $password = $_SERVER['R,Oalx:ZQ.0P'];
+
+    $pdo = new PDO($dsn, $username, $password);
