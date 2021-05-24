592cc4956cc3d2e5f0df473ecc75331e54bdbf87
NielsDewolfPXL <43777476+NielsDewolfPXL@users.noreply.github.com>
2021-05-24 14:52:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -35,103 +35,11 @@
                 //echo "<img class='d-block w-100' src='https://cloudgroup7.s3.amazonaws.com/assets/images/$image'/></div>";
             //}     
         //}
-        //foreach($images as $key => $img){
-                //echo ($key == 0 or $key == 1 or $key == 2) ? '<div class="carousel-item active">' : '<div class="carousel-item">';
-                //echo "<img class='d-block w-100' src='https://cloudgroup7.s3.amazonaws.com/assets/images/$img'/></div>";
-        //}
-        <!-- Carousel wrapper -->
-        <div
-          id="carouselBasicExample"
-          class="carousel slide carousel-fade"
-          data-mdb-ride="carousel"
-        >
-          <!-- Indicators -->
-          <div class="carousel-indicators">
-            <button
-              type="button"
-              data-mdb-target="#carouselBasicExample"
-              data-mdb-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-mdb-target="#carouselBasicExample"
-              data-mdb-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-mdb-target="#carouselBasicExample"
-              data-mdb-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-
-          <!-- Inner -->
-          <div class="carousel-inner">
-            <!-- Single item -->
-            <div class="carousel-item active">
-              <img
-                src="https://cloudgroup7.s3.amazonaws.com/assets/images/$images[0]"
-                class="d-block w-100"
-                alt="..."
-              />
-             </div>
-
-            <!-- Single item -->
-            <div class="carousel-item">
-              <img
-                src="https://cloudgroup7.s3.amazonaws.com/assets/images/$images[1]"
-                class="d-block w-100"
-                alt="..."
-              />             
-            </div>
-
-            <!-- Single item -->
-            <div class="carousel-item">
-              <img
-                src="https://cloudgroup7.s3.amazonaws.com/assets/images/$images[2]"
-                class="d-block w-100"
-                alt="..."
-              />
-             </div>
-          </div>
-          <!-- Inner -->
-
-          <!-- Controls -->
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-mdb-target="#carouselBasicExample"
-            data-mdb-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-mdb-target="#carouselBasicExample"
-            data-mdb-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-<!-- Carousel wrapper -->
-    ?>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+        foreach($images as $key => $img){
+                echo ($key == 0 or $key == 1 or $key == 2) ? '<div class="carousel-item active">' : '<div class="carousel-item">';
+                echo "<img class='d-block w-100' src='https://cloudgroup7.s3.amazonaws.com/assets/images/$img'/></div>";
+        }
+       
 
         <?php
         // Use 'composer install' to get dependencies
